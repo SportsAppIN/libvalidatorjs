c060e2fc56c1a206ac572d60843ead26b589f863
Himanshu Nain <himanshu_bt2k16@dtu.ac.in>
2022-06-30 12:23:07 +0530

    add empty string check

--- index.js
+++ index.js
@@ -3,62 +3,72 @@
 const validator = require('validator');
 
 module.exports = {
-	...validator,
-	isFunction,
-	revMapOfOrDefault,
-	mapOfOrDefault,
-	setOfOrDefault,
-	isNullOrUndefined,
-	isNull,
-	isUndefined,
-	isSet,
-	isWeakMap,
-	isWeakSet,
-	isMap,
-	isObject,
-	getPositiveNumberOrDefault,
-	getNullIfUndefined,
-	getNegativeNumberOrDefault,
-	getArrayOrDefault,
-	getObjectOrDefault,
-	getStringOrDefault,
-	getNumberOrDefault,
-	isEmptyArray,
-	isEmptyArrayOrNull,
-	isArrayOrNull,
-	isStringOrNull,
-	isString,
-	isArray,
-	isNumber,
-	isNumberOrNull,
-	isPositiveNumberOrNull,
-	isNegativeNumberOrNull,
-	isNegativeNumber,
-	isPositiveNumber
+  ...validator,
+  isFunction,
+  revMapOfOrDefault,
+  mapOfOrDefault,
+  setOfOrDefault,
+  isNullOrUndefined,
+  isNull,
+  isUndefined,
+  isSet,
+  isWeakMap,
+  isWeakSet,
+  isMap,
+  isObject,
+  getPositiveNumberOrDefault,
+  getNullIfUndefined,
+  getNegativeNumberOrDefault,
+  getArrayOrDefault,
+  getObjectOrDefault,
+  getStringOrDefault,
+  getNumberOrDefault,
+  isArrayOrNull,
+  isStringOrNull,
+  isString,
+  isArray,
+  isNumber,
+  isNumberOrNull,
+  isPositiveNumberOrNull,
+  isNegativeNumberOrNull,
+  isNegativeNumber,
+  isPositiveNumber,
+  isEmptyString,
+  isEmptyArray,
+  isEmptyStringOrNull,
+  isEmptyArrayOrNull
 };
 
+function isEmptyStringOrNull(val) {
+  return isNull(val) || isEmptyString(val);
+}
+
+function isEmptyString(val) {
+  return isString(val) && val.trim().length < 1;
+}
+
 function isFunction(val) {
-	return typeof val === 'function';
+  return typeof val === 'function';
 }
 
 function revMapOfOrDefault(val, defaultVal) {
-	if(!isObject(val))
-		return defaultVal;
+  if (!isObject(val))
+    return defaultVal;
 
-	const map = new Map();
-	const keys = Object.keys(val);
-	keys.forEach(key => map.set(val[key], key));
-	return map;
+  const map = new Map();
+  const keys = Object.keys(val);
+  keys.forEach(key => map.set(val[key], key));
+  return map;
 }
 
 function mapOfOrDefault(val, defaultVal) {
-	if(!isObject(val))
-		return defaultVal;
+  if (!isObject(val))
+    return defaultVal;
 
-	const map = new Map();
-	const keys = Object.keys(val);
-	keys.forEach(key => map.set(key, val[key]));
-	return map;
+  const map = new Map();
+  const keys = Object.keys(val);
+  keys.forEach(key => map.set(key, val[key]));
+  return map;
 }
 
 /**
@@ -68,119 +78,119 @@ function mapOfOrDefault(val, defaultVal) {
  * @return {Set<any>|*}
  */
 function setOfOrDefault(val, defaultVal) {
-	return isArray(val) ? new Set(val) : isString(val) ? new Set(val.split('')) : defaultVal;
+  return isArray(val) ? new Set(val) : isString(val) ? new Set(val.split('')) : defaultVal;
 }
 
 function isNullOrUndefined(val) {
-	return isUndefined(val) || isNull(val);
+  return isUndefined(val) || isNull(val);
 }
 
 function isNull(val) {
-	return val === null;
+  return val === null;
 }
 
 function isUndefined(val) {
-	return val === undefined;
+  return val === undefined;
 }
 
 function isSet(val) {
-	return val instanceof Set;
+  return val instanceof Set;
 }
 
 function isWeakSet(val) {
-	return val instanceof WeakSet;
+  return val instanceof WeakSet;
 }
 
 function isMap(val) {
-	return val instanceof Map;
+  return val instanceof Map;
 }
 
 function isWeakMap(val) {
-	return val instanceof WeakMap;
+  return val instanceof WeakMap;
 }
 
 function isObject(val) {
-	return !isNull(val) &&
-		!isSet(val)  &&
-		!isMap(val) &&
-		!isWeakMap(val) &&
-		!isWeakSet(val) &&
-		!isArray(val) &&
-		typeof val === 'object';
+  return !isNull(val) &&
+    !isSet(val) &&
+    !isMap(val) &&
+    !isWeakMap(val) &&
+    !isWeakSet(val) &&
+    !isArray(val) &&
+    typeof val === 'object';
 }
 
 function getObjectOrDefault(val, defaultVal) {
-	return isObject(val) ? val : defaultVal;
+  return isObject(val) ? val : defaultVal;
 }
 
 function getNullIfUndefined(val) {
-	return isUndefined(val) ? null : val;
+  return isUndefined(val) ? null : val;
 }
 
 function isEmptyArray(val) {
-	return isArray(val) && val.length === 0;
+  return isArray(val) && val.length === 0;
 }
 
 function isEmptyArrayOrNull(val) {
-	return isNull(val) || isEmptyArray(val);
+  return isNull(val) || isEmptyArray(val);
 }
 
 function isArray(val) {
-	return Array.isArray(val);
+  return Array.isArray(val);
 }
 
 function isArrayOrNull(val) {
-	return isNull(val) || isArray(val);
+  return isNull(val) || isArray(val);
 }
 
 function getArrayOrDefault(val, defaultVal) {
-	return isArray(val) ? val : defaultVal;
+  return isArray(val) ? val : defaultVal;
 }
 
 function isString(val) {
-	return typeof val === 'string';
+  return typeof val === 'string';
 }
 
 function isStringOrNull(val) {
-	return isNull(val) || isString(val);
+  return isNull(val) || isString(val);
 }
 
 function getStringOrDefault(val, defaultVal) {
-	return isString(val) ? val : defaultVal;
+  return isString(val) ? val : defaultVal;
 }
 
 function isNumber(val) {
-	return !Number.isNaN(val) && val !== Infinity && typeof val === 'number';
+  return !Number.isNaN(val) && val !== Infinity && typeof val === 'number';
 }
 
 function isNumberOrNull(val) {
-	return isNull(val) || isNumber(val);
+  return isNull(val) || isNumber(val);
 }
 
 function getNumberOrDefault(val, defaultVal) {
-	return isNumber(val) ? val : defaultVal;
+  return isNumber(val) ? val : defaultVal;
 }
 
 function isPositiveNumber(val) {
-	return isNumber(val) && val > 0;
+  return isNumber(val) && val > 0;
 }
 
 function isPositiveNumberOrNull(val) {
-	return isNull(val) || isPositiveNumber(val);
+  return isNull(val) || isPositiveNumber(val);
 }
 
 function getPositiveNumberOrDefault(val, defaultVal) {
-	return isPositiveNumber(val) ? val : defaultVal;
+  return isPositiveNumber(val) ? val : defaultVal;
 }
 
 function isNegativeNumber(val) {
-	return isNumber(val) && val < 0;
+  return isNumber(val) && val < 0;
 }
 
 function isNegativeNumberOrNull(val) {
-	return isNull(val) || isNegativeNumber(val);
+  return isNull(val) || isNegativeNumber(val);
 }
 
 function getNegativeNumberOrDefault(val, defaultVal) {
-	return isNegativeNumber(val) ? val : defaultVal;
+  return isNegativeNumber(val) ? val : defaultVal;
 }
